0e030cc6f06b69b843926f7615f89ec14dfc9884
/dev/null <65429873+gXLg@users.noreply.github.com>
2024-12-11 10:03:43 +0100

    Create worker-node.sh

---
+++ worker-node.sh
@@ -0,0 +1,17 @@
+# 1. Connect to PoE
+# 2. Boot and run this script
+
+# update system
+sudo apt update
+sudo apt upgrade -y
+sudo apt install nodejs npm -y
+
+# disable wifi
+sudo sh -c "echo 'dtoverlay=pi3-disable-wifi' >> /boot/firmware/config.txt"
+
+# connecting to nfs
+mkdir -p ~/shared
+sudo sh -c "echo '192.168.69.1:/home/$(whoami)/shared  /home/$(whoami)/shared  nfs  rw,defaults,nofail,x-systemd.automount,vers=4,noatime  0  0' >> /etc/fstab"
+
+# reboot
+sudo reboot
