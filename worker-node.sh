32912a7dea608b7924698e8cacf3279c86efbfbf
/dev/null <65429873+gXLg@users.noreply.github.com>
2024-12-18 17:40:19 +0100

    Update worker-node.sh

--- worker-node.sh
+++ worker-node.sh
@@ -13,5 +13,22 @@ sudo sh -c "echo 'dtoverlay=pi3-disable-wifi' >> /boot/firmware/config.txt"
 mkdir -p ~/shared
 sudo sh -c "echo '192.168.69.1:/home/$(whoami)/shared  /home/$(whoami)/shared  nfs  rw,defaults,nofail,x-systemd.automount,vers=4,noatime  0  0' >> /etc/fstab"
 
+# add worker to systemd
+cat << EOF | sudo tee /etc/systemd/system/gxlg-worker.service
+[Unit]
+Description=gXLg Cluster Worker
+After=network.target
+RequiresMountsFor=/home/$(whoami)/shared
+
+[Service]
+User=$(whoami)
+WorkingDirectory=/home/$(whoami)/shared/gxlg-cluster
+ExecStart=/usr/bin/node worker.js
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl enable gxlg-worker.service
+
 # reboot
 sudo reboot
