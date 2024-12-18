19b70b49d5acdfafce38e880caca9bdc2a4eec6d
/dev/null <65429873+gXLg@users.noreply.github.com>
2024-12-18 19:24:34 +0100

    Update worker-node.sh

--- worker-node.sh
+++ worker-node.sh
@@ -11,19 +11,50 @@ sudo sh -c "echo 'dtoverlay=pi3-disable-wifi' >> /boot/firmware/config.txt"
 
 # connecting to nfs
 mkdir -p ~/shared
-sudo sh -c "echo '192.168.69.1:/home/$(whoami)/shared  /home/$(whoami)/shared  nfs  rw,defaults,nofail,x-systemd.automount,vers=4,noatime  0  0' >> /etc/fstab"
+
+cat << EOF | sudo tee /etc/systemd/system/wait-for-network.service
+[Unit]
+Description=Wait for local network
+
+[Service]
+Type=oneshot
+ExecStart=/bin/sh -c 'until ping -c1 192.168.69.1; do sleep 1; done'
+
+[Install]
+WantedBy=nfs-client.target
+EOF
+sudo systemctl enable wait-for-network.service
+
+cat << EOF | sudo tee /etc/systemd/system/home-$(whoami)-shared.mount
+[Unit]
+Description=Mount NFS
+After=NetworkManager-wait-online.service
+Requires=NetworkManager-wait-online.service
+
+[Mount]
+What=192.168.69.1:/home/$(whoami)/shared
+Where=/home/$(whoami)/shared
+Type=nfs
+Options=rw,defaults,noatime,vers=4
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl enable home-$(whoami)-shared.mount
 
 # add worker to systemd
 cat << EOF | sudo tee /etc/systemd/system/gxlg-worker.service
 [Unit]
 Description=gXLg Cluster Worker
-After=network.target
-RequiresMountsFor=/home/$(whoami)/shared
+After=home-$(whoami)-shared.mount
+Requires=home-$(whoami)-shared.mount
 
 [Service]
-User=$(whoami)
+User=gxlg
 WorkingDirectory=/home/$(whoami)/shared/gxlg-cluster
 ExecStart=/usr/bin/node worker.js
+KillSignal=SIGINT
+SendSIGKILL=no
 
 [Install]
 WantedBy=multi-user.target
