9496f1dc64035de85c7d6fc7f4e36a01a9ac93cf
gXLg <65429873+gXLg@users.noreply.github.com>
2025-11-23 21:55:38 +0100

    Install Node.js using NVM and update service command
    Replaced Node.js installation method with NVM and updated service exec command.

--- worker-node.sh
+++ worker-node.sh
@@ -4,7 +4,14 @@
 # update system
 sudo apt update
 sudo apt upgrade -y
-sudo apt install nodejs npm -y
+
+# install Node.js v20.19.5 (LTS)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install 20.19.5
+npm install -g npm@latest
 
 # disable wifi
 sudo sh -c "echo 'dtoverlay=pi3-disable-wifi' >> /boot/firmware/config.txt"
@@ -52,7 +59,7 @@ Requires=home-$(whoami)-shared.mount
 [Service]
 User=$(whoami)
 WorkingDirectory=/home/$(whoami)/shared/gxlg-cluster
-ExecStart=/usr/bin/node worker.js
+ExecStart=/bin/bash -c "node worker.js"
 KillSignal=SIGINT
 KillMode=process
 SendSIGKILL=no
